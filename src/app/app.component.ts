9a61db2a052431495daba52f342efdc6cb87a65f
GIC-GAURAV <gaurav.kakran@gridinfocom.com>
2023-02-10 17:52:53 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,190 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+declare var flowy:any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drag-and-drop';
+  lists = [
+    {
+      title : "New visitor",
+      description : "Triggers when somebody visits a specified page",
+      value : "1",
+      icon : "assets/eye.svg"
+    },
+    {
+      title : "Action is performed",
+      description : "Triggers when somebody performs a specified action",
+      value : "2",
+      icon : "assets/action.svg"
+    },
+    {
+      title : "Time has passed 1",
+      description : "Triggers after a specified amount of time",
+      value : "3",
+      icon : "assets/time.svg"
+    },
+    {
+      title : "Error prompt 1",
+      description : "Triggers when a specified error happens",
+      value : "4",
+      icon : "assets/error.svg"
+    },
+    {
+      title : "Garuav kakran",
+      description : "Hey, I'm gaurav kakran",
+      value : "5",
+      icon : "assets/error.svg"
+    }
+  ]
+
+  aclick:any = false;
+  noinfo:any = false; 
+  
+  closeItem :any 
+  blocklist:any
+  propertiesItem:any 
+  propwrapItem:any 
+  removeBlockItem :any 
+  canvas : any 
+  showHTML : any = false
+  @ViewChild("tempblock2", { read: ViewContainerRef, static:true }) tempblock2:any = null;
+  @ViewChild("tempblock", { read: ViewContainerRef }) tempblock:any = null;
+  constructor(){
+
+  }
+  ngOnInit(){
+    this.blocklist = document.getElementById("blocklist")
+    this.propertiesItem = document.getElementById("properties")
+    this.propwrapItem = document.getElementById("propwrap")
+    this.removeBlockItem  = document.getElementById("removeblock")
+    this.canvas = document.getElementById("canvas")
+      console.log("this.blocklist", this.blocklist)
+      this.blocklist.innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>';
+      flowy = new flowy(this.canvas, this.drag, this.release, this.snapping);
+      this.addEventListenerMulti("click", this.disabledClick, false, ".side");
+      addEventListener("mousedown", this.beginTouch, false);
+      addEventListener("mousemove", this.checkTouch, false);
+      addEventListener("mouseup", this.doneTouch, false);
+      this.addEventListenerMulti("touchstart", this.beginTouch, false, ".block");
+  }
+
+
+
+    rightcard:any = false;
+    // tempblock : any;
+    // tempblock2 : any;
+   
+
+    addEventListenerMulti(type:any, listener:any, capture:any, selector:any) {
+        var nodes = document.querySelectorAll(selector);
+        for (var i = 0; i < nodes.length; i++) {
+            nodes[i].addEventListener(type, listener, capture);
+        }
+    }
+
+    snapping(drag:any, first:any) {
+        var grab = drag.querySelector(".grabme");
+        grab.parentNode.removeChild(grab);
+        var blockin = drag.querySelector(".blockin");
+        blockin.parentNode.removeChild(blockin);
+        if (drag.querySelector(".blockelemtype").value == "1") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/eyeblue.svg'><p class='blockyname'>New visitor</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When a <span>new visitor</span> goes to <span>Site 1</span></div>";
+        } else if (drag.querySelector(".blockelemtype").value == "2") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/actionblue.svg'><p class='blockyname'>Action is performed</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>Action 1</span> is performed</div>";
+        } else if (drag.querySelector(".blockelemtype").value == "3") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/timeblue.svg'><p class='blockyname'>Time has passed</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>10 seconds</span> have passed</div>";
+        } else if (drag.querySelector(".blockelemtype").value == "4") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/errorblue.svg'><p class='blockyname'>Error prompt</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>When <span>Error 1</span> is triggered</div>";
+        } else if (drag.querySelector(".blockelemtype").value == "5") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/databaseorange.svg'><p class='blockyname'>New database entry</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Add <span>Data object</span> to <span>Database 1</span></div>";
+        } else if (drag.querySelector(".blockelemtype").value == "6") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/databaseorange.svg'><p class='blockyname'>Update database</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Update <span>Database 1</span></div>";
+        } else if (drag.querySelector(".blockelemtype").value == "7") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/actionorange.svg'><p class='blockyname'>Perform an action</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Perform <span>Action 1</span></div>";
+        } else if (drag.querySelector(".blockelemtype").value == "8") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/twitterorange.svg'><p class='blockyname'>Make a tweet</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Tweet <span>Query 1</span> with the account <span>@alyssaxuu</span></div>";
+        } else if (drag.querySelector(".blockelemtype").value == "9") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/logred.svg'><p class='blockyname'>Add new log entry</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Add new <span>success</span> log entry</div>";
+        } else if (drag.querySelector(".blockelemtype").value == "10") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/logred.svg'><p class='blockyname'>Update logs</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Edit <span>Log Entry 1</span></div>";
+        } else if (drag.querySelector(".blockelemtype").value == "11") {
+            drag.innerHTML += "<div class='blockyleft'><img src='assets/errorred.svg'><p class='blockyname'>Prompt an error</p></div><div class='blockyright'><img src='assets/more.svg'></div><div class='blockydiv'></div><div class='blockyinfo'>Trigger <span>Error 1</span></div>";
+        }
+        return true;
+    }
+
+    drag(block?:any) {
+        if(this.tempblock2){
+          // console.log("Block", block, this.tempblock2)
+          block.classList.add("blockdisabled");
+          this.tempblock2 = block;
+        }
+    }
+    release() {
+        if (this.tempblock2) {
+            this.tempblock2.classList.remove("blockdisabled");
+        }
+    }
+    disabledClick = () =>{
+        let navactive:any = document.querySelector(".navactive");
+        navactive.classList.add("navdisabled");
+        navactive.classList.remove("navactive");
+        navactive.classList.add("navactive");
+        navactive.classList.remove("navdisabled");
+        if (navactive.getAttribute("id") == "triggers") {
+            this.blocklist.innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="1"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/eye.svg"></div><div class="blocktext">                        <p class="blocktitle">New visitor</p><p class="blockdesc">Triggers when somebody visits a specified page</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="2"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Action is performed</p><p class="blockdesc">Triggers when somebody performs a specified action</p></div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="3"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/time.svg"></div><div class="blocktext">                        <p class="blocktitle">Time has passed</p><p class="blockdesc">Triggers after a specified amount of time</p>          </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="4"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                    <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Error prompt</p><p class="blockdesc">Triggers when a specified error happens</p>              </div></div></div>';
+        } else if (navactive.getAttribute("id") == "actions") {
+            this.blocklist.innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="5"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">New database entry</p><p class="blockdesc">Adds a new entry to a specified database</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="6"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/database.svg"></div><div class="blocktext">                        <p class="blocktitle">Update database</p><p class="blockdesc">Edits and deletes database entries and properties</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="7"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/action.svg"></div><div class="blocktext">                        <p class="blocktitle">Perform an action</p><p class="blockdesc">Performs or edits a specified action</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="8"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/twitter.svg"></div><div class="blocktext">                        <p class="blocktitle">Make a tweet</p><p class="blockdesc">Makes a tweet with a specified query</p>        </div></div></div>';
+        } else if (navactive.getAttribute("id") == "loggers") {
+            this.blocklist.innerHTML = '<div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="9"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Add new log entry</p><p class="blockdesc">Adds a new log entry to this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="10"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/log.svg"></div><div class="blocktext">                        <p class="blocktitle">Update logs</p><p class="blockdesc">Edits and deletes log entries in this project</p>        </div></div></div><div class="blockelem create-flowy noselect"><input type="hidden" name="blockelemtype" class="blockelemtype" value="11"><div class="grabme"><img src="assets/grabme.svg"></div><div class="blockin">                  <div class="blockico"><span></span><img src="assets/error.svg"></div><div class="blocktext">                        <p class="blocktitle">Prompt an error</p><p class="blockdesc">Triggers a specified error</p>        </div></div></div>';
+        }
+    }
+
+    
+close(){
+  this.closeItem.addEventListener("click", () =>{
+    if (this.rightcard) {
+        this.rightcard = false;
+        this.propertiesItem.classList.remove("expanded");
+        setTimeout(() =>{
+             this.propwrapItem.classList.remove("itson"); 
+        }, 300);
+         this.tempblock.classList.remove("selectedblock");
+    } 
+ });
 }
+
+removeBlock(){
+  this.removeBlockItem.addEventListener("click", () =>{
+    // flowy.deleteBlocks();
+   });
+}
+
+beginTouch = (event:any) =>{
+    let aclick = true;
+    let noinfo = false;
+    if (event.target.closest(".create-flowy")) {
+        noinfo = true;
+    }
+}
+checkTouch = (event:any) =>{
+    let aclick = false;
+}
+doneTouch =  (event:any) => {
+    if (event.type === "mouseup" && this.aclick && !this.noinfo) {
+      if (!this.rightcard && event.target.closest(".block") && !event.target.closest(".block").classList.contains("dragging")) {
+            this.tempblock = event.target.closest(".block");
+            this.rightcard = true;
+            document.getElementById("properties")!.classList.add("expanded");
+            document.getElementById("propwrap")!.classList.add("itson");
+            this.tempblock.classList.add("selectedblock");
+       } 
+    }
+}
+
+}
+
